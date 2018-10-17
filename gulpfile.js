c6ada87db5a9f85a58e8d1242ce08a111681c569
Cristhian Ramirez <cristhian.ramirez@universomascotas.co>
2018-10-17 11:54:34 -0500

    Estilo: Crea task para m inificar html en dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const gulp = require('gulp');
 const zip = require('gulp-zip');
+const htmlmin = require('gulp-htmlmin');
  
 gulp.task('zip_code', () =>
     gulp.src('src/*.htm')
+    	.pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(zip('um.zip'))
         .pipe(gulp.dest('dist'))
 );
